7a4848c2cf34e8ce3e80355284be39cb5dc029e4
Francesco De Carlo <decarlof@gmail.com>
2016-05-07 22:02:46 -0500

    added mock to solve an issue with import astropy. See https://github.com/data-exchange/dxchange/issues/26

--- doc/conf.py
+++ doc/conf.py
@@ -328,7 +328,8 @@ MOCK_MODULES = [
     'numpy', 'scipy', 'scipy.misc', 'scipy.optimize', 'h5py', 'ctypes',
     'matplotlib', 'matplotlib.pylab', 'tifffile', 'EdfFile', 'netCDF4',  
     'spefile', 'scipy.ndimage', 'pywt', 'scikit-image', 'skimage',
-    'skimage.io', 'skimage.filter', 'skimage.morphology', 'DM3lib']
+    'skimage.io', 'skimage.filter', 'skimage.morphology', 'DM3lib', 'dxchange',
+    'dxchange.reader']
 
 
 for mod_name in MOCK_MODULES:
