86f6388d8f1727199c322e8495d3dc97db4ce9fc
ravescovi <ravescovi@gmail.com>
2018-01-16 10:28:12 -0600

    New Macros

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
         'License :: BSD-3',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
